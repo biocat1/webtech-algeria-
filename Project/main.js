f8edc891520adc41a160f5c8d0261001fe00d2b7
TMHansY <muhsinhasni@gmail.com>
2025-01-02 23:06:13 +0800

    Add files via upload

---
+++ Project/main.js
@@ -0,0 +1,19 @@
+function toMenu(){
+    window.location="menu.html";
+}
+function toStatus(){
+    window.location="status.html";
+}
+function toContact(){
+    window.location="contact.html";
+}
+function toOrder(){
+    window.location="orderPage.html";
+}
+function toHome(){
+    window.location="homepage.html";
+}
+function modeChange() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
