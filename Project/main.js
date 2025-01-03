faffc5ab17d5dd98bc3b3533fc814900b333c242
TMHansY <muhsinhasni@gmail.com>
2025-01-03 23:49:12 +0800

    Update main.js

--- Project/main.js
+++ Project/main.js
@@ -7,7 +7,7 @@ function toStatus(){
 function toContact(){
     window.location="contact.html";
 }
-function toOrder() {
+function toOrder(){
     window.location="orderPage.html";
 }
 function toHome(){
@@ -15,5 +15,16 @@ function toHome(){
 }
 function modeChange() {
     var element = document.body;
-    element.classList.toggle("dark-mode");
+    element.classList.toggle("dark-mode"); /* ignore dulu still experiment*/
  }
+
+function showStatus(){
+    let d = new Date();
+    if(d.getHours() >= 23 || d.getHours() <= 9){
+        let element = document.getElementById("statusShow");
+        element.classList.remove("open");  // Remove open class
+        element.classList.add("close");  // Add close class
+
+        document.getElementById("show").innerHTML ="Our shop is closed for now"
+    }
+}
