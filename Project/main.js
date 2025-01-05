cfc6b04b1b64265c0cfaafa169efe472b636fd34
TMHansY <muhsinhasni@gmail.com>
2025-01-05 09:11:35 +0800

    Update main.js

--- Project/main.js
+++ Project/main.js
@@ -20,11 +20,11 @@ function modeChange() {
 
  function showStatus() {
     let d = new Date();
-    let currentHour = d.getHours();
+    let currentHour = d.getHours(),currentDay = d.getDay();
 
     let element = document.getElementById("statusShow");
 
-    if (currentHour >= 9 && currentHour < 23) {
+   if (currentHour >= 9 && currentHour < 23 && currentDay != 0) {
         // Shop is open
         element.classList.remove("close"); // Remove close class
         element.classList.add("open");    // Add open class
